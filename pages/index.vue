a4fc35511d92223bd3f896526a7a7f9138ea5dcf
Albert Puigsech <albert@puigsech.com>
2024-09-13 19:58:15 +0200

    Implement dynamic url -- {term}

--- pages/index.vue
+++ pages/index.vue
@@ -10,11 +10,14 @@ const terms = ref([]);
 onMounted(async () => {
     try {
         const response = await $fetch('/api/terms');
-        terms.value = response;
+        terms.value = response.map(term => ({
+            ...term,
+            url: term.url
+                .replace("{term}", encodeURIComponent(term.term).replace(/%20/g, '+'))
+        }));
     } catch (error) {
         console.error("Error fetching terms:", error);
     }
 });
-
 
 </script>
